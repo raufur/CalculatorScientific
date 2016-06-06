80910ad4a23658867588fa2a36598d35df08b921
Raufur Rahman <r.raufur2000@gmail.com>
2016-06-06 16:11:53 +0600

    Initial commit

---
+++ app/src/main/java/com/raufur/calculatorscientific/lands.java
@@ -0,0 +1,27 @@
+package com.raufur.calculatorscientific;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class lands extends Activity implements OnClickListener{
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+        setContentView(com.raufur.calculatorscientific.R.layout.land);
+        
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
